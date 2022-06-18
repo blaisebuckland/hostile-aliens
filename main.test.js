452940f8ef730ca1ce3f421184e5cb789c9e2329
Blaise Buckland <bucklandblaise@gmail.com>
2022-06-18 18:21:44 +0100

    Plan tests

---
+++ main.test.js
@@ -0,0 +1,9 @@
+// test if , when we create a ship, it has the values we expect
+
+// test if, when we have created a ship and then we hit it, is loses the correct number of points
+
+// test if, when a ship goes down to zero points, it gains the destroyed class
+
+// test if, when the mothership is destroyed, the game over modal is displayed
+
+// test if, when all ships are destroyed, the game over modal is displayed
