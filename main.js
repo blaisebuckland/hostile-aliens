a61664a832a0a5030ba8e3377450fbc955bfce5a
Blaise Buckland <bucklandblaise@gmail.com>
2022-06-18 18:21:32 +0100

    Delete unused code

--- main.js
+++ main.js
@@ -1,33 +1,19 @@
-/* HOSTILE ALIENS
-
-OOP 
-
-MAKING OUR SHIPS - CLASS OF SHIP - EXTEND FOR SHIP TYPE. PARAMETERS - STATRTING POINTS, LOSS INCREMENT
-
-BUTTON - ON CLICK HIT A RANDOM SHIP - RANDOMISER FOR ALL SHIPS THAT WILL NOT SELECT  A SHIP AT 0 POINT ALREADY
-
-WHEN HIT, DECREASE POINTS OR GAME OVER IF MOTHER SHIP 
-
-UPDATE INNER HTML OF HIT SHIP
-
-GAMEOVER WHEN ALL ZERO OR MOTHER SHIP DEFEATED
-
-INSTRUCTIONS BUTTON AND PLAY AGAIN
-
-WINNING MODAL*/
-
+const mothershipSection = document.querySelector(".mothership");
+const defenceSection = document.querySelector(".defence");
+const attackSection = document.querySelector(".attack");
 const fireBtn = document.querySelector("#fire-btn");
 const instructionsBtn = document.querySelector("#instructions-btn");
 const instructionsModal = document.querySelector("#instructions-modal-container");
 const replayBtnMain = document.querySelector("#replay-btn-main");
-const winningModal = document.querySelector("#winning-modal-container");
 const replayBtnModal = document.querySelector("#replay-btn-modal");
+const winningModal = document.querySelector("#winning-modal-container");
 const closeWinningModalBtn = document.querySelector("#winning-modal-close");
 const closeInstructionsModalBtn = document.querySelector("#instructions-modal-close");
 
 let ships = [];
 let shipsInPlay = [];
 let randomShip = "";
+
 class Ship {
     constructor(shipType, shipName, totalPoints, attackDamage) {
         this.shipType = shipType;
@@ -35,13 +21,6 @@ class Ship {
         this.totalPoints = totalPoints;
         this.attackDamage = attackDamage;
     }
-
-    // buildShip(numberOfShips) {
-    //     for (let index = 0; index < numberOfShips; index++) {
-    //         ships.push(`${this.shipType}${index}`);
-    //         document.querySelector(`.${this.shipType}`).innerHTML += `<div class="ship" id="${this.shipType}${index}"><h5>${this.shipName}</h5> <p>Points: ${this.totalPoints}</p></div>`
-    //     }
-    // }
 
     hitShip(index) {
         this.totalPoints -= this.attackDamage;
@@ -53,52 +32,34 @@ class Ship {
         } 
     }
 }
-
+// Should this be a method in the Ship class? Or is that weird because we don't need to be able to call it for individual instances of the class?
 const buildShips = (indexStart, shipType, shipName, totalPoints, attackDamage, numberOfShips) => {
     for (let index = indexStart; index < (indexStart + numberOfShips); index++) {
         ships.push(new Ship(shipType, shipName, totalPoints, attackDamage));
         document.querySelector(`.${shipType}`).innerHTML += `<div class="ship" id="${shipType}${index}"><h5>${shipName}</h5> <p>Points: ${totalPoints}</p></div>`;
     }
 }
-// Create instances of the ship class
-// const motherShip = new Ship("mothership", "Mother Ship", 100, 9);
-// const defenceShip = new Ship("defence", "Defence Ship", 80, 10);
-// const attackShip = new Ship("attack", "attack Ship", 45, 12);
-
-// Use the ship objects to render the ships
-// motherShip.buildShip(1);
-// defenceShip.buildShip(5);
-// attackShip.buildShip(8);
 
 // all hit points set to 50 for testing, change this back to correct numbers!
 const startGame = () => {
-    buildShips(0, "mothership", "Mother Ship", 100, 50, 1);
-    buildShips(1, "defence", "Defence Ship", 80, 50, 5);
-    buildShips(6, "attack", "Attack Ship", 45, 50, 8);
+    buildShips(0, "mothership", "Mother Ship", 100, 9, 1);
+    buildShips(1, "defence", "Defence Ship", 80, 10, 5);
+    buildShips(6, "attack", "Attack Ship", 45, 12, 8);
     shipsInPlay = [...ships];
 }
 
 startGame();
 
-// NEED TO ADD A WAY OF NOT HITTING SHIPS ALREADY AT 0
 const getRandomIndex = () => {
-    // console.log(ships.length)
     const randomIndex = Math.floor(Math.random()* shipsInPlay.length);
-    // console.log(randomIndex)
-    // randomShip = ships[randomIndex];
-    // console.log(randomShip);
     return randomIndex;
-    
 }
 
 const gameOver = () => {
-    console.log("GAME OVER, YOU WIN")
     const displayWinningModal = () => winningModal.style.display = "block";
     setTimeout(displayWinningModal, 800);
-    // winningModal.style.display = "block";
 }
 
-// do we want to win if all ships are destroyed inc the mothership, or either mothership or all others? Have done former for now
 const checkPoints = () => {
     if (ships[0].totalPoints === 0) gameOver();
     if (ships.every(ship => ship.totalPoints === 0)) gameOver();
@@ -106,14 +67,14 @@ const checkPoints = () => {
 
  const resetGame = () => {
     winningModal.style.display = "none";
-    document.querySelector(".mothership").innerHTML = "";
-    document.querySelector(".defence").innerHTML = "";
-    document.querySelector(".attack").innerHTML = "";
+    mothershipSection.innerHTML = "";
+    defenceSection.innerHTML = "";
+    attackSection.innerHTML = "";
     ships = [];
     shipsInPlay = [];
     startGame();
  }
-
+// get this function out of the event listener
  fireBtn.addEventListener("click", () => {
     let hitShipIndex = getRandomIndex();
     // if (ships[hitShipIndex].totalPoints === 0) hitShipIndex = getRandomIndex();
